cddb8823fa818ab9931b6ec7b40780d9dc980919
FunyaMain <funyamain@gmail.com>
2026-04-28 23:42:06 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHAT_ID = os.getenv("CHAT_ID")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
