c23fa57925cad06a67b371598da72a6df016baa4
FunyaMain <funyamain@gmail.com>
2026-04-29 12:08:09 +0300

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,1 +1,12 @@
+from aiogram import Router, types
+from db.queries import create_user
 
+router = Router()
+
+@router.message(commands=["start"])
+async def start(msg: types.Message):
+    await create_user(msg.from_user.id)
+
+    await msg.answer(
+        "👋 Привет!\nДобро пожаловать в Dating Travel 🌍"
+    )
