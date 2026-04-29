65d74b42a2878adc9f3c15fb086af2dbf19ccc4b
FunyaMain <funyamain@gmail.com>
2026-04-29 12:07:47 +0300

    Update roles.py

--- bot/services/roles.py
+++ bot/services/roles.py
@@ -1,1 +1,9 @@
+from db.queries import get_user_role
+from config import ADMIN_IDS
 
+async def is_admin(user_id):
+    return user_id in ADMIN_IDS
+
+async def is_moderator(user_id):
+    role = await get_user_role(user_id)
+    return role == "moderator"
