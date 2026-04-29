c6e0f8d1b1a87b02f38798f0d70cb8b34604ea58
FunyaMain <funyamain@gmail.com>
2026-04-29 12:09:26 +0300

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,1 +1,7 @@
+from aiogram import Router, types
 
+router = Router()
+
+@router.message(lambda m: m.text == "🖼 Фото анонимок")
+async def admin_photo(msg: types.Message):
+    await msg.answer("📸 Отправьте фото для анонимных сообщений")
