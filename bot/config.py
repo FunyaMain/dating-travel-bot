0927f00861eb1028043dbe0512763a9719e78698
FunyaMain <funyamain@gmail.com>
2026-04-29 11:50:28 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,26 +1,7 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-# 🤖 BOT
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-
-# 👑 Админ (если используешь)
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-
-
-# 📢 Канал / чат
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-CHAT_ID = os.getenv("CHAT_ID")
-
-
-# 🗄 База данных (Railway)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-# 🛠 АДМИН-ЛОГ (ДОБАВЛЕНО)
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
-ADMIN_TOPIC_ID = int(os.getenv("ADMIN_TOPIC_ID", "0"))
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
