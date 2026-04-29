323b82a66e8c6e7a0a0e8346045489dd8f0e1e44
FunyaMain <funyamain@gmail.com>
2026-04-29 11:56:23 +0300

    Delete bot/handlers directory

--- bot/handlers/anon.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
-router = Router()
-
-
-@router.message(Command("anon"))
-async def anon_cmd(message: types.Message):
-    await message.answer("💬 Анонимные вопросы скоро будут доступны.")
