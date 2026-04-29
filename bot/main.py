7b8ae509e5e5e5b8840151946a8518ceee232060
FunyaMain <funyamain@gmail.com>
2026-04-29 09:20:57 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,23 +1,8 @@
-import asyncio
 from aiogram import Bot, Dispatcher
-from bot.config import BOT_TOKEN
-from bot.db import init_db
-
 from bot.handlers import start, register, anon
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+dp = Dispatcher()
 
-    await init_db()
-
-    dp.include_router(start.router)
-    dp.include_router(register.router)
-    dp.include_router(anon.router)
-
-    print("Bot started")
-
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+dp.include_router(start.router)
+dp.include_router(register.router)
+dp.include_router(anon.router)
