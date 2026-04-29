baa95f2eb73085d3084a358730d1f5bf4b5f8215
FunyaMain <funyamain@gmail.com>
2026-04-29 12:08:35 +0300

    Update profile.py

--- bot/handlers/profile.py
+++ bot/handlers/profile.py
@@ -1,1 +1,7 @@
+from aiogram import Router, types
 
+router = Router()
+
+@router.message(lambda m: m.text == "👤 Мой профиль")
+async def profile(msg: types.Message):
+    await msg.answer("📌 Ваш профиль (заглушка)")
