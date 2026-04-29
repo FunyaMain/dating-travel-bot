db979a20ab4a4f35b874a2c8ec5c4928f56a7433
FunyaMain <funyamain@gmail.com>
2026-04-29 12:09:59 +0300

    Update moderator.py

--- bot/handlers/moderator.py
+++ bot/handlers/moderator.py
@@ -1,1 +1,7 @@
+from aiogram import Router, types
 
+router = Router()
+
+@router.message(lambda m: m.text == "📢 Публикации")
+async def post(msg: types.Message):
+    await msg.answer("📢 Выберите топик и создайте пост")
